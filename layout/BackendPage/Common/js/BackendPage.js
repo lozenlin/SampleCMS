fcb1e70bd7ad945912d3ed31976cc3f15a34109d
Lozen Lin <lozenlin@gmail.com>
2017-10-14 18:20:45 +0800

    切版製作: 後台首頁(未完);

---
+++ layout/BackendPage/Common/js/BackendPage.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="jquery-3.2.1.js"/>
+
+// Global Settings
+$(function () {
+    $(".main-navbar").click(function () {
+        smoothUp();
+    });
+});
+
+function smoothUp(id) {
+    if (id == undefined)
+        id = "";
+
+    $("html, body").animate({
+        scrollTop: (id == "") ? 0 : $("#" + id).offset().top
+    }, 500);
+}
