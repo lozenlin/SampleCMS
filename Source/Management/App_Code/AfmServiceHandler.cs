4b3c1f78fdc29b6437da56c5155f222529eea6de
Lozen Lin <lozenlin@gmail.com>
2017-11-25 17:43:17 +0800

    後端作業選項管理(未完): 圖示挑選功能(未完), remove directories and files;

--- Source/Management/App_Code/AfmServiceHandler.cs
+++ Source/Management/App_Code/AfmServiceHandler.cs
@@ -175,7 +175,158 @@ public class AfmRemoveDirOrFiles : AfmServiceHandlerAbstract
     {
         AfmResult result = null;
 
-        result = BuildResultOfError("not yet");
+        if (afmRequest.items == null || afmRequest.items.Length == 0)
+        {
+            result = BuildResultOfError("items is invalid");
+            return result;
+        }
+
+        foreach (string item in afmRequest.items)
+        {
+            string ext = Path.GetExtension(item);
+
+            if (ext != "")
+            {
+                // as file
+                bool isFile = IsFileExists(item);
+
+                if (isFile)
+                {
+                    if(!RemoveFile(item))
+                    {
+                        result = BuildResultOfError(string.Format("remove file {0} failed", item));
+                        return result;
+                    }
+                }
+                else
+                {
+                    bool isDir = IsDirectoryExists(item);
+
+                    if (isDir)
+                    {
+                        if (!RemoveDirectory(item))
+                        {
+                            result = BuildResultOfError(string.Format("remove directory {0} failed", item));
+                            return result;
+                        }
+                    }
+                    else
+                    {
+                        result = BuildResultOfError(string.Format("file {0} does not exist", item));
+                        return result;
+                    }
+                }
+            }
+            else
+            {
+                // as directory
+                bool isDir = IsDirectoryExists(item);
+
+                if (isDir)
+                {
+                    if (!RemoveDirectory(item))
+                    {
+                        result = BuildResultOfError(string.Format("remove directory {0} failed", item));
+                        return result;
+                    }
+                }
+                else
+                {
+                    bool isFile = IsFileExists(item);
+
+                    if (isFile)
+                    {
+                        if (!RemoveFile(item))
+                        {
+                            result = BuildResultOfError(string.Format("remove file {0} failed", item));
+                            return result;
+                        }
+                    }
+                    else
+                    {
+                        result = BuildResultOfError(string.Format("directory {0} does not exist", item));
+                        return result;
+                    }
+                }
+            }
+        }
+
+        result = BuildResultOfSuccess();
+
         return result;
+    }
+
+    private bool IsFileExists(string item)
+    {
+        string fileFullName = GetListDir(item, AfmFileType.file);
+        return File.Exists(fileFullName);
+    }
+
+    private bool RemoveFile(string item)
+    {
+        string fileFullName = GetListDir(item, AfmFileType.file);
+        FileInfo fi = new FileInfo(fileFullName);
+
+        if (!fi.Exists)
+        {
+            return false;
+        }
+
+        try
+        {
+            fi.Delete();
+
+            //新增後端操作記錄
+            empAuth.InsertBackEndLogData(new BackEndLogData()
+            {
+                EmpAccount = c.GetEmpAccount(),
+                Description = string.Format("．FileManager remove file　．ListType[{0}]　．file[{1}]", c.qsListType, item),
+                IP = c.GetClientIP()
+            });
+        }
+        catch (Exception ex)
+        {
+            c.LoggerOfUI.Error("", ex);
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool IsDirectoryExists(string item)
+    {
+        string dirFullName = GetListDir(item, AfmFileType.dir);
+        return Directory.Exists(dirFullName);
+    }
+
+    private bool RemoveDirectory(string item)
+    {
+        string dirFullName = GetListDir(item, AfmFileType.dir);
+        DirectoryInfo di = new DirectoryInfo(dirFullName);
+
+        if (!di.Exists)
+        {
+            return false;
+        }
+
+        try
+        {
+            di.Delete(true);
+
+            //新增後端操作記錄
+            empAuth.InsertBackEndLogData(new BackEndLogData()
+            {
+                EmpAccount = c.GetEmpAccount(),
+                Description = string.Format("．FileManager remove directory　．ListType[{0}]　．dir[{1}]", c.qsListType, item),
+                IP = c.GetClientIP()
+            });
+        }
+        catch (Exception ex)
+        {
+            c.LoggerOfUI.Error("", ex);
+            return false;
+        }
+
+        return true;
     }
 }
